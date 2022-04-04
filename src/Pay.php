f872b3f699419bebd73df6076db3b1567901c252
李铭昕 <715557344@qq.com>
2022-04-04 13:08:20 +0800

    Added HttpClientFactory. (#18)
    * Added HttpClientFactoryInterface
    
    * Added HttpClientFactory

--- src/Pay.php
+++ src/Pay.php
@@ -55,10 +55,12 @@ class Pay
      */
     protected function bootstrapHttpClient(): void
     {
-        BigPay::set(
-            HttpClientInterface::class,
-            $this->container->get(ClientFactory::class)->create($this->config['http'] ?? [])
-        );
+        if ($this->container->has(HttpClientFactoryInterface::class) && $factory = $this->container->get(HttpClientFactoryInterface::class)) {
+            $client = $factory->create($this->container, $this->config);
+        } else {
+            $client = $this->container->get(ClientFactory::class)->create($this->config['http'] ?? []);
+        }
+        BigPay::set(HttpClientInterface::class, $client);
     }
 
     /**
